89d7ae61ab36d61e3850abdc71b4231ffc7245d7
Wael Yehdego <wael@waels-mbp.attlocal.net>
2018-09-13 20:43:26 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>This is wlyhdg and welcome to my new Gatsby site.</p>
+    <p>I will become a phenomenal developer.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
